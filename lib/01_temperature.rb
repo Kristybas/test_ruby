e2695c62ea0357bffcdf1f5d51d573482c929e6f
Kristybas <kristybasila@gmail.com>
2023-10-09 19:40:27 +0200

    ajout ex temperatur

---
+++ lib/01_temperature.rb
@@ -0,0 +1,37 @@
+def ftoc 
+  def ftoc(fahrenheit)
+       celsius = (fahrenheit -  celsius) = (32)* 5/9
+          
+       return celsius
+    end
+
+    def ctof(celsius)
+        celsius_to_fahrenheit(celsius)
+        (celsius * 9/5) + 32
+      end
+          
+    
+       def if ftoc (32) == (0)
+
+       end
+          
+        def if ftoc(212) ==(100)
+
+        end
+
+       def if ftoc(98.6) ==(37)
+
+       end
+
+       def if ftoc(68) ==(20)
+
+       end
+
+       def ctof (32)
+        return (0) 
+       end
+
+
+       def ctof (100) 
+        retun(212)
+       end
